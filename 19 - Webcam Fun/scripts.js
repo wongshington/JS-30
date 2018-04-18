f6b9fdbf6e0cb7433a70e32957b9fd84d9d5718e
Jesse <jnwong17@gmail.com>
2018-04-17 23:15:03 -0700

    Add color effect function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -27,7 +27,16 @@ function paintToCanvas() {
   canvas.height = height;
 
   return setInterval(() => {
+    // we return it so we can get the id of the interval if we want to clear it later
     ctx.drawImage(video, 0, 0, width, height);
+
+    let pixels = ctx.getImageData(0,0,width, height);
+    // allows us to select the pixel data
+
+    pixels = redEffect(pixels);
+    // makes the color effect on the pixels
+
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 
 }
@@ -38,7 +47,23 @@ function takePhoto() {
   // plays the camera shutter sound
 
   const data = canvas.toDataURL('image/jpeg');
-  console.log(data);
+  // console.log(data);
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "cool-view");
+  // link.textContent = "download Image";
+
+  link.innerHTML = `<img src="${data}" alt="that guy" />`;
+  strip.insertBefore(link, strip.firsChild);
+}
+
+function redEffect(pixels){
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * .5; //blue
+  }
+  return pixels;
 }
 
 getVideo();
