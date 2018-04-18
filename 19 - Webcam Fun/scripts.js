891d3ae60d27a1f7d9f0abec9ce94608213dd488
Jesse <jnwong17@gmail.com>
2018-04-17 20:27:17 -0700

    Render video player

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,18 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  // returns a promise object
+  .then(localMediaStream => {
+
+    console.log(localMediaStream);
+    video.src = window.URL.createObjectURL(localMediaStream);
+    // converts it to a readable type
+
+    video.play();
+  });
+}
+
+getVideo();
